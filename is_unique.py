9d426479c07347899b180e36a1629ea5e738d9e1
Hannah Chong <hhchong97@gmail.com>
2020-01-30 23:52:24 +0000

    added notes for code

--- is_unique.py
+++ is_unique.py
@@ -1,18 +1,25 @@
-def is_unique(str):
+#Implement an algorithm to determine if a string has all unique characters. 
 
+
+def is_unique(str):
+    #create a seen dictionary, for fast lookup if a char has been seen
     seen = {}
 
+    #in order to return False, otherwise leaving this out will return None for empty string
     if len(str) == 0:
         return False
 
+    #iterate through str and check if it's in the dictionary
     for char in str:
 
         if char in seen:
             return False
 
+        #if not seen, add to set dictionary
         else:
             seen[char] = seen.get(char, 0) + 1
-
+    #if we check all the char in str and they were all not in seen, means it is unique
+    #return True
     return True
 
 print (is_unique('aaaaa'))
