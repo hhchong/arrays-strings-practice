1b6db9828b0a6c39d4217675d9a5606e7a5983f8
Hannah Chong <hhchong97@gmail.com>
2020-02-11 03:37:58 +0000

    O(1) space for zero matrix

--- zero_matrix.py
+++ zero_matrix.py
@@ -34,7 +34,7 @@
 #use dictionaries to store the location of y and the location of x?
 
 
-def convert_zero(m):
+def convert_zero(matrix):
 
     #find len width of matrix
 
@@ -46,23 +46,69 @@ def convert_zero(m):
     # for item in dict.keys, m[dict.keys][range width] -> 0
     #for item in row, m[range width][item] -> 0
 
-    width = len(m[0])
+    # width = len(m[0])
 
-    hm = {}
+    # hm = {}
 
-    for i,j in enumerate(m):
-        for k,l in enumerate(j):
-            if l == 0:
-                hm[i] = k
+    # for i,j in enumerate(m):
+    #     for k,l in enumerate(j):
+    #         if l == 0:
+    #             hm[i] = k
 
-    for i,j in enumerate(m):
-        for k,l in enumerate(j):
-            if i in hm.keys():
-                m[i][k] = 0
-            elif k in hm.values():
-                m[i][k] = 0
+    # for i,j in enumerate(m):
+    #     for k,l in enumerate(j):
+    #         if i in hm.keys():
+    #             m[i][k] = 0
+    #         elif k in hm.values():
+    #             m[i][k] = 0
 
-    return m
+    # return m
+
+    #time complexity is O(M x N) space O(M+N), use the matrix itself as data storage to get space to O(1)
+
+    #instead we can use the first cell of every row and col as a flag, set to 0
+
+    m = len(matrix) #2
+    n = len(matrix[0]) #3
+
+    #use left most to mark rows to convert, use top most to mark cols to convert
+    row_zero = False
+    for i in range(m):
+        if matrix[i][0] == 0:
+            row_zero = True
+
+    col_zero = False
+    for j in range(n):
+        if matrix[0][j] == 0:
+            col_zero = True
+
+    #mark where row/col should be flagged as 0 rows
+    for i in range(1, m):
+        for j in range(1, n):
+            if matrix[i][j] == 0:
+                matrix[i][0] = 0
+                matrix[0][j] = 0
+
+    #now actually convert them to 0
+    for i in range(1, m):
+        if matrix[i][0] == 0:
+            for j in range(1, n):
+                matrix[i][j] = 0
+    for j in range(1, n):
+        if matrix[0][j] == 0:
+            for i in range(1, m):
+                matrix[i][j] = 0
+    # if col_zero:
+    #     for j in range(n):
+    #         matrix[0][j] = 0
+
+    # if row_zero:
+    #     for i in range(m):
+    #         matrix[i][0] = 0
+    return matrix
+
+
+
 
 
 print(convert_zero([[1,2,3,4],[5,2,1,0],[6,1,8,6]]))
